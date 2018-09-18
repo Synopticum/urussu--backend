8bd929279348ba22f8d6ce630abc5b2505ef7c38
Sergey Novikov <sergey.novikov@macys.com>
2018-09-19 01:57:32 +0300

    Added object GET route

--- services/objects/object/index.js
+++ services/objects/object/index.js
@@ -8,11 +8,32 @@ module.exports = async function (fastify, opts) {
 
 async function registerRoutes(fastify, opts) {
     fastify.route({
+        method: 'GET',
+        url: '/objects/:object',
+        beforeHandler: fastify.auth([fastify.verifyVkAuth]),
+        handler: getObject
+    });
+
+    fastify.route({
         method: 'PATCH',
         url: '/objects/:object',
         beforeHandler: fastify.auth([fastify.verifyVkAuth]),
         handler: updateObject
     });
+
+    async function getObject(request, reply) {
+        let objectId = request.params.object;
+
+        try {
+            let object = await ObjectModel.findOne({ _id: mongoose.Types.ObjectId(objectId) });
+            reply.type('application/json').code(200);
+            return object;
+        } catch (e) {
+            reply.type('application/json').code(500);
+            console.error(e);
+            return { error: `Unable to get object: error when finding in db`}
+        }
+    }
 
     async function updateObject(request, reply) {
         let object = request.body;
