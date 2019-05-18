a7d0ae7f9c09219d6200c0709f7261825398465d
Sergey Novikov <synopticum@gmail.com>
2019-05-18 15:26:26 +0300

    Check rights when removing a dot

--- services/dot/delete.js
+++ services/dot/delete.js
@@ -2,6 +2,7 @@ const { s3 } = require('../../config/aws');
 const DotModel = require('../../db/dot.model');
 const CommentModel = require('../../db/comment.model');
 const verifyVkAuth = require('../authenticate/verifyVkAuth');
+const { currentUser } = require('../authenticate/request.helpers');
 
 module.exports = async function (fastify, opts) {
     fastify
@@ -18,19 +19,38 @@ async function registerRoutes(fastify, opts) {
     async function remove(request, reply) {
         let dotId = request.params.dot;
 
-        try {
-            await removePhotos(request, reply, dotId);
-            await removeModel(request, reply, dotId);
-            await removeComments(request, reply, dotId);
+        if (await canRemove(request, dotId)) {
+            try {
+                await removePhotos(request, reply, dotId);
+                await removeModel(request, reply, dotId);
+                await removeComments(request, reply, dotId);
 
-            reply.type('application/json').code(200);
-            return {};
-        } catch (e) {
-            reply.type('application/json').code(500);
-            console.error(e);
-            return { error: `Unable to delete a dot: unknown error when deleting`}
+                reply.type('application/json').code(200);
+                return {};
+            } catch (e) {
+                reply.type('application/json').code(500);
+                console.error(e);
+                return { error: `Unable to delete a dot: unknown error when deleting`}
+            }
+        } else {
+            reply.type('application/json').code(400);
+            return { error: `Unable to remove a dot: you have no rights`}
         }
     }
+}
+
+async function canRemove(request, dotId) {
+    let userId = await currentUser.getId(request);
+    let isAdmin = await currentUser.isAdmin(request);
+
+    if (userId) {
+        let dotModel = await DotModel.findOne({ id: dotId });
+        let dotAuthorId = dotModel._doc.authorId;
+
+        return dotAuthorId === userId || isAdmin;
+    }
+
+    return false;
 }
 
 async function removePhotos(request, reply, dotId) {
